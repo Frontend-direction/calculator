cd8ee9a995d08306f95a25d42448a74440222351
Vova Pankiv <vovapankiw@gmail.com>
2018-09-09 14:40:35 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
-const digits = document.querySelectorAll('.digits-and-operators');
-digits.forEach(el =>el.addEventListener('click', pressDigitAndOperator));    
+const digitsAndOperator = document.querySelectorAll('.digits-and-operators');
+digitsAndOperator.forEach(el =>el.addEventListener('click', pressDigitAndOperator));    
 
 function pressDigitAndOperator (ev) {
-    const display = document.querySelector('.calculator .disp');
-    display.value += ev.target.innerText;
+		const display = document.querySelector('.calculator .disp');
+		display.value += ev.target.innerText;
+}
+
+const sqroot = document.querySelector('.calculator .sqroot');
+sqroot.addEventListener('click', squareroot);
+
+function squareroot() {
+		const display = document.querySelector('.calculator .disp');
+		display.value =  Math.sqrt(parseFloat(eval(display.value)));
+}
+
+const ln = document.querySelector('.calculator .ln');
+ln.addEventListener('click', natLog);
+
+function natLog() {
+		const display = document.querySelector('.calculator .disp');
+		display.value =  Math.log(parseFloat(eval(display.value)));
+}
+
+const exp = document.querySelector('.calculator .exp');
+exp.addEventListener('click', expon);
+
+function expon() {
+		const display = document.querySelector('.calculator .disp');
+		display.value =  Math.exp(parseFloat(eval(display.value)));
 }
 
 const result = document.querySelector('.calculator .result');
